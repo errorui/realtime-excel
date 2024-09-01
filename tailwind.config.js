ecc1b8bdad01092dc525cbb8bc5c8ce519c2ee10
CodeSpec <online.codespec@gmail.com>
2024-09-01 12:33:33 +0530

    tailwind 1.0

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,14 @@
 // /** @type {import('tailwindcss').Config} */
 // module.exports = {
 //   darkMode: ["class"],
-//   content: [
-//     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-//     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-//     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-//   ],
+//
+// content: [
+//   "//     ./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+//   "//     ./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+//   "//     ./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+//   "//",
+//   "./node_modules/@nextui-org/theme/dist/components/(dropdown|table|menu|divider|popover|button|ripple|spinner|checkbox|spacer).js"
+// ],
 //   prefix: "",
 //   theme: {
 //     container: {
@@ -74,22 +77,21 @@
 //   },
 // };
 
-
 /** @type {import('tailwindcss').Config} */
-import {nextui} from "@nextui-org/react";
+import { nextui } from "@nextui-org/react";
 module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
- 
+
     // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
-    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
   },
   darkMode: "class",
   plugins: [nextui()],
-}
+};
